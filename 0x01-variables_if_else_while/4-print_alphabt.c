5fb81d63f70e96196ee93c2cdb54673687754514
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-09 14:09:23 +0200

    added file 4 for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		if (x != 'q' && x != 'e')
+		{
+		putchar(x);
+		x++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
