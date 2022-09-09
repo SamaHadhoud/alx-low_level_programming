17b1359288d4940185ea40fdcb4e22df20bcf344
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-09 14:27:35 +0200

    added file 8 for task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	x = 48;
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	x = 97;
+	while(x <= 'f')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
