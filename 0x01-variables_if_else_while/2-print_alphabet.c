1412bdc010b9c21dfd5bb6b1683050ee2f02e84e
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-09 14:01:00 +0200

    added file 2 for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
