98834189817ecc64fb889551924b5033cac088af
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-09 14:24:17 +0200

    added file 7 for task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
