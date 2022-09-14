4042a1fd667f5c8bc7aa1928b29856db3e2c9f93
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-14 21:34:04 +0200

    added files for task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,23 @@
 
 void print_to_98(int n)
 {
-	int r1, r2;
-
-	for (; n < 98; n++)
+	if (n < 98)
 	{
-		if (n < 10)
+		for (; n <= 98; n++)
 		{
-			_putchar('0' + n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 		}
-		else
-		{
-			r1 = n % 10;
-			r2 = n / 10;
-			_putchar('0' + r2);
-			_putchar('0' + r1);
-		}
-		_putchar(',');
-		_putchar(' ');
 	}
-	_putchar('0' + 9);
-	_putchar('0' + 8);
-	_putchar('\n');
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("/n");
 }
