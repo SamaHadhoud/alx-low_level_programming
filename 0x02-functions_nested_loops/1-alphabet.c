a11130fee657ae92e204554a39e7783a0ea0314e
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-14 19:44:08 +0200

    added files for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
+	return;
 }
