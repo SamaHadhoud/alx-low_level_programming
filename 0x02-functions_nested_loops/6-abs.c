4d87857abc1f4415eeba5768f6f282a91c0f9de9
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-14 20:34:57 +0200

    added files for task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computer absolute value of integer
+ * @int: integer
+ * Return: absolute value
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -37,6 +37,14 @@ int _islower(int c);
 int _isalpha(int c);
 
 /**
+ * _abs - computer absolute value of integer
+ * @int: integer
+ * Return: absolute value
+ */
+
+int _abs(int);
+
+/**
  * print_sign - print sign of number
  * @n: number to check
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
