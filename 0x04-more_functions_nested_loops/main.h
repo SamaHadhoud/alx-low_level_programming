63f568d90f1e5939055fa7c59f1dd06d3e6bc77c
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-16 19:19:11 +0200

    added files for task 6

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -49,3 +49,9 @@ void print_most_numbers(void);
  */
 void more_numbers(void);
 
+/**
+ * print_line - draw a stight line
+ * @n: the number of time the character '_' should be print
+ */
+void print_line(int n);
+
