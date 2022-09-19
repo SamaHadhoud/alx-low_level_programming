19cb62a33288c73899970e64f810376eab9a7622
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 20:33:18 +0200

    added files for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - a string in reverse
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int n, x, i;
+	char tmp;
+
+	n = strlen(s);
+	x = n - 1;
+	for (i = 0; i <= n / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + x);
+		*(s + x) = tmp;
+		x--;
+	}
+	_putchar('\n');
+}
