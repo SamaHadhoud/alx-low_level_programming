8f6a472e42b3c4b61cf4e7fe7a965c9bbab3f0e9
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 19:44:18 +0200

    added files for task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two intergers
+ * @a: first value to be swaped
+ * @b: second value to be swaped
+ */
+void swap_int(int *a, int *b);
+{
+	int x;
+
+	x = a;
+	*a = b;
+	*b = x;
+}
