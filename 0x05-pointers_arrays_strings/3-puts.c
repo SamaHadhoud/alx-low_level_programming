847aba2f0b3d63b5c1d98a681c80f03e1e97ce6b
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 20:06:25 +0200

    added files for task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - print a string then endline
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	while (*s != '\0')
+		_putchar(*s++);
+	_putchar('\n');
+	return (x);
+}
