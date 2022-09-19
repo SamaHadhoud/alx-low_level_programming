7097412910c1c58c62518470dc1865f6e08e5ac4
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 19:57:04 +0200

    added files for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: the string
+ * Return: the length value
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = strlen(s);
+	return (x);
+}
