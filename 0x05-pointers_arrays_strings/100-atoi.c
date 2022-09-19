d66b7a07ae026880e60636ac349867ccf2933937
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 21:44:12 +0200

    added files for advanced task 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _atoi - convert string to an integer
+ * @s: the string
+ * Return: the integer
+ */
+int _atoi(char *s)
+{
+	int i, n, x;
+
+	n = strlen(src);
+	x = 1;
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] == '-')
+			x * = -1;
+		else if (s[i] == '+')
+			x * = 1;
+		else
+		{
+			x *= s[i];
+			x * = 10;
+			if (x < 0)
+				x--;
+			else
+				x++;
+		}
+	}
+	if (n > 0)
+		return (x / 10);
+	else
+		return (0);
+}
+
