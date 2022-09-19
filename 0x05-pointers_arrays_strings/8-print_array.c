f5ad488ceeddb386531aa74ec499c642fef4d63c
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-19 21:17:56 +0200

    added files for task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	n--;
+	for (i = 0; i < n; i++)
 	{
 		printf("%d, ", *(a + i));
 	}
-	_putchar(*(a + (n - 1)));
+
+	printf("%d", *(a + n);
 }
