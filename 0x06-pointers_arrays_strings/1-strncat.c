b8d44853f5e508c7440a87bd17681a5a30e13b1c
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-22 13:44:04 +0200

    added files for task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concantenates two strings
+ * @src: source
+ * @dest: destination
+ * @n: the number of bytes from the src
+ * Return: pointer to the destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = strlen(dest);
+	j = 0;
+	while ((src[j] != '\n') && (j < n))
+	{
+		dest[i++] = src[j++];
+	}
+	dest[i] = '\n';
+	return (dest);
+}
