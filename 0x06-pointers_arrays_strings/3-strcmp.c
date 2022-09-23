b37f7c280d95715915291b03b9cbd272365b1cb6
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-23 12:58:02 +0200

    fix betty style

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,9 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second stringn
- * Return: an integer less than, equal to, or greater than zero if s1 is found, respectively, to be less than, to match, or be greater than s2. 
+ * Return: an integer less than, equal to, or greater than zero
+ * if s1 is found, respectively, to be less than, to match,
+ * or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
