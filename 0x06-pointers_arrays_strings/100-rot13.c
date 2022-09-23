3c45103ff7bba1d473a257f5b136f862de9fa2df
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-09-23 16:22:03 +0200

    added files for advanced task 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rot13 - encodes a string into rot13
+ * @str: string to edit
+ * Return: string edited
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	int from[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	int to[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; from[j] != '\0'; j++)
+		{
+			if (str[i] == from[j])
+			{
+				str[i] = to[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
