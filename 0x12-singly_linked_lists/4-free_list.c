5356ae02dee5ffc3a4a238f530de9c2130fa4a4b
SamaHadhoud <sama.hadhoud@ejust.edu.eg>
2022-10-20 23:31:43 +0200

    initial commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ * @head: linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		 return;
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
